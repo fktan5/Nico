710afe717c66f412fa6bb0e7ccee652b0060c0b7
tan5 fk <mail@fktan5.net>
2013-11-08 00:19:40 +0900

    deprecated ...

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Nico
 ====
 
 LoveLive Chrome Extension
+
+Popupからtumblr経由で画像を表示させる
--- popup.js
+++ popup.js
@@ -9,15 +9,21 @@ var QUERY = 'lovelive';
 
 var loveliveGenerator = {
 
-	searchOnAjaxGoogle_: 'http://ajax.googleapis.com/ajax/services/search/images?' + 
+	searchOnAjax_: 'http://ajax.googleapis.com/ajax/services/search/images?' + 
 		'q=' + encodeURIComponent(QUERY) + 
 		'&v=1.0&safe=on',
 
 	requestLoveLive: function(){
-		var req = new XMLHttpRequest();
-		req.open("GET", this.searchOnAjaxGoogle_, true);
-		req.onload = this.showImages_.bind(this);
-		req.send(null);
+		console.log(this.searchOnAjax_);
+		$.ajax({
+			type: 'GET',
+			url: this.searchOnAjax_,
+			dataType: 'jsonp',
+			success: function(json){
+				console.log(json);
+				
+			}
+		});
 	},
 
  	showImages_: function(e) {
@@ -28,25 +34,6 @@ var loveliveGenerator = {
 	}
 };
 
-var kittenGenerator = {
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
-
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
-  }
-};
-
 document.addEventListener('DOMContentLoaded', function () {
+	loveliveGenerator.requestLoveLive();
 });
